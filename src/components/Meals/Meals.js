bdefbe8e97b50aafdc84e5fbaefdbfc86a8bc67d
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-10 22:12:57 +0400

    Implemented Meals, AvailableMeals, MealsSummary components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = _ => {
+  return (
+    <>
+      <MealsSummary/>
+      <AvailableMeals/>
+    </>
+  );
+};
+
+export default Meals;
