8ea677b4af22f7b809880afb6bae379a39515938
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-11 20:00:06 +0400

    Show total items in a header cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,20 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
+import {useContext} from 'react';
+import CartContext from '../../store/CartContext';
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+  const totalCountOfItems = cartContext.items
+    .reduce((accum, item) => accum + item.amount, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon/>
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{3}</span>
+      <span className={classes.badge}>{totalCountOfItems}</span>
     </button>
   )
 };
