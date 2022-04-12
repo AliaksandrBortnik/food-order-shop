ecff6e875c9597e37780ce6affca2dc347a5ee20
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-12 09:23:59 +0400

    Add useReducer to modify cart state in Context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,46 @@
 import CartContext from './CartContext';
+import {useReducer} from 'react';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      // TODO: check if item already exists
+      return {
+        items: [...state.items, action.item],
+        totalAmount: state.totalAmount + action.item.price * action.item.amount
+      };
+    case 'REMOVE_ITEM':
+      return;
+    default:
+      return state;
+  }
+};
 
 const CartProvider = props => {
-  const addItemToCart = item => {};
-  const removeItemFromCart = id => {};
+  const [state, dispatchAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCart = item => {
+    dispatchAction({
+      type: 'ADD_ITEM',
+      item
+    });
+  };
+  
+  const removeItemFromCart = id => {
+    dispatchAction({
+      type: 'REMOVE_ITEM',
+      id
+    });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: state.items,
+    totalAmount: state.totalAmount,
     addItem: addItemToCart,
     removeItem: removeItemFromCart
   };
