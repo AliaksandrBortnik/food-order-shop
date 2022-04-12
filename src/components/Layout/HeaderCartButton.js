5a50a552455574a8013ea4cea9d0b94c7b8ef81f
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-12 12:33:14 +0400

    Animate cart button on each cart change

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import CartContext from '../../store/CartContext';
 
 const HeaderCartButton = (props) => {
   const cartContext = useContext(CartContext);
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
+
   const totalCountOfItems = cartContext.items
     .reduce((accum, item) => accum + item.amount, 0);
 
+  const btnClasses = `${classes.button} ${btnIsAnimated ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return
+    }
+
+    setBtnIsAnimated(true);
+
+    const timerId = setTimeout(
+      () => setBtnIsAnimated(false),
+      300
+    );
+
+    return () => clearTimeout(timerId);
+  }, [cartContext.items]);
+
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon/>
       </span>
